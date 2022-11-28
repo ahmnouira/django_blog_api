428c1a26e489ea47b0842b9234b600955efd045f
Ahmed Nouira <ahmnouira@gmail.com>
2022-11-28 18:12:00 +0100

    feat: added custom user account

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    name = models.CharField(null=True, blank=True, max_length=100)
+
+    def __str__(self) -> str:
+        return self.email
